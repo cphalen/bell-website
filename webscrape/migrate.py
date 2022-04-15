b1e3d69f5fd987003d9b0bac121f03af3f17abcd
Ben Demers <ben.n.demers@gmail.com>
2022-04-14 23:49:16 -0400

    update profile format and add socials + css

--- webscrape/migrate.py
+++ webscrape/migrate.py
@@ -4,7 +4,7 @@ import json
 # new markdown files in the content directory of the Hugo site so Hugo
 # can render the historical data
 
-migrations = ["honorary", "2014", "2015", "2016", "2017", "2018", "2019", "2020", "2021"]
+migrations = ["Honorary", "2014", "2015", "2016", "2017", "2018", "2019", "2020", "2021"]
 
 for migration in migrations:
     with open(f"old_data/{migration}.json") as file_in:
@@ -13,9 +13,10 @@ for migration in migrations:
             with open(f"../content/{migration}/{profile['code']}.md", "w+") as file_out:
                 file_out.write("---\n")
                 file_out.write(f"name: \"{profile['name']}\"\n")
+                file_out.write(f"class: \"{migration}\"\n")
                 file_out.write(f"image: \"{profile['image']}\"\n")
                 file_out.write(f"email: \"{profile['socials']['email']}\"\n")
-                file_out.write(f"code: \"{profile['code']}\"\n")
+                file_out.write(f"slug: \"{profile['code']}\"\n")
                 file_out.write(f"github: \"{profile['socials']['github']}\"\n")
                 file_out.write(f"twitter: \"{profile['socials']['twitter']}\"\n")
                 file_out.write(f"linkedin: \"{profile['socials']['linkedin']}\"\n")
